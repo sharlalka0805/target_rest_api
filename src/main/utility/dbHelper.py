127481ebb6a64c5d4f8eb7ef91354953574f2136
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-05-25 15:18:42 -0400

    Update dbHelper.py

--- src/main/utility/dbHelper.py
+++ src/main/utility/dbHelper.py
@@ -1,30 +1,27 @@
-import json
 import sqlite3 as sql
+
 
 class dbHelper:
 
-    def initialSetUp(self):
+    def createDatabase(self):
         try:
             with sql.connect("questionAnswer.db") as con:
                 cur = con.cursor()
                 # Create the basic tables
                 # AppLogger
-                cur.execute(""" CREATE TABLE IF NOT EXISTS APPLOGGER (model_name text
-                                         ,question_asked_time integer
-                                         ,question_text text
-                                         ,answer_sent_time integer
-                                         ,answer_text text
-                                         ,created_time integer
+                cur.execute(""" CREATE TABLE IF NOT EXISTS APP_LOGGER (model_name text
+                                         ,action_type text
+                                         ,action_details text
                                          ,created_by text
-                                         ,modified_time integer
-                                         ,modified_by text) """)
+                                         ,created_time integer """)
 
                 # Models
                 cur.execute(""" CREATE TABLE IF NOT EXISTS NLP_Models (name text,tokenizer text,model text)""")
 
                 # QuestionAnswer
                 cur.execute(
-                    """ CREATE TABLE IF NOT EXISTS Question_Answer (modelName text,question text,context text,answer text,createdDate integer)""")
+                    """CREATE TABLE IF NOT EXISTS Question_Answer (modelName text,question text,context text,
+                    answer text,createdDate integer)""")
 
                 con.commit()
                 return "Initial DB setup completed"
@@ -34,7 +31,28 @@ class dbHelper:
         finally:
             con.close()
 
-# Create method to upload default data in NLP Models table using XML
+    def insertintialdata(self):
+        try:
+            with sql.connect("questionAnswer.db") as con:
+                cur = con.cursor()
+                # Create the basic tables
+
+                cur.execute(""" INSERT INTO NLP_Models(name,tokenizer,model) 
+                                SELECT 'bert-base-multilingual-uncased'
+                                        ,'bert-base-multilingual-uncased'
+                                        ,'bert-base-multilingual-uncased' 
+                                WHERE NOT EXISTS(
+                                SELECT 1 FROM NLP_Models WHERE name = 'bert-base-multilingual-uncased'); """)
+
+                con.commit()
+                return "Initial DB setup completed"
+        except:
+            con.rollback()
+            return "Exception occurred while initial DB setup"
+        finally:
+            con.close()
+
+    # Create method to upload default data in NLP Models table using XML
 
     def listModels(self):
         try:
@@ -50,10 +68,9 @@ class dbHelper:
             con.close()
         return rows
 
-
-    def addModel(self,name, tokenizer, model):
+    def addModel(self, name, tokenizer, model):
         try:
-            print('Inside dbHelper.addModel : ',model,tokenizer,name)
+            print('Inside dbHelper.addModel : ', model, tokenizer, name)
             with sql.connect("questionAnswer.db") as con:
                 cur = con.cursor()
                 # Introduce for in case you need to persist an ad-hoc lis
@@ -66,27 +83,26 @@ class dbHelper:
         finally:
             con.close()
 
-
-    def deleteModel(self,modelName):
+    def deleteModel(self, modelName):
         try:
-            print('Inside dbHelper.deleteModel for model = ',modelName)
+            print('Inside dbHelper.deleteModel for model = ', modelName)
             with sql.connect("questionAnswer.db") as con:
                 cur = con.cursor()
 
                 cur.execute("DELETE FROM NLP_Models WHERE NAME = :modelName", {'modelName': modelName})
                 con.commit()
-                print('Inside dbHelper.deleteModel for model = ', modelName , "Model deleted successfully")
+                print('Inside dbHelper.deleteModel for model = ', modelName, "Model deleted successfully")
                 return "Model deleted successfully"
         except:
             con.rollback()
-            return "Exception occured while trying to delete model for model = "+modelName
+            return "Exception occured while trying to delete model for model = " + modelName
         finally:
             con.close()
 
-
-    def getRecentlyAnsweredQuestionsList(self,modelName, startTime, endTime):
+    def getRecentlyAnsweredQuestionsList(self, modelName, startTime, endTime):
         try:
-            print('Inside dbHelper.getRecentlyAnsweredQuestionsList for model = ', modelName,' startTime = ' , startTime , ' endTime = ',endTime)
+            print('Inside dbHelper.getRecentlyAnsweredQuestionsList for model = ', modelName, ' startTime = ',
+                  startTime, ' endTime = ', endTime)
             with sql.connect("questionAnswer.db") as con:
                 con.row_factory = sql.Row
                 cur = con.cursor()
@@ -103,22 +119,24 @@ class dbHelper:
         finally:
             con.close()
 
-    def saveRecentlyAnsweredQuestion(self,questionAnswer):
+    def saveRecentlyAnsweredQuestion(self, questionAnswer):
         try:
-            print('Inside dbHelper.getRecentlyAnsweredQuestionsList for model = ', questionAnswer.modelName, ' question = ',
-                  questionAnswer.question, ' context = ', questionAnswer.context ,' answer = ',questionAnswer.answer,'timeStamp = ',questionAnswer.timestamp)
+            print('Inside dbHelper.getRecentlyAnsweredQuestionsList for model = ', questionAnswer.modelName,
+                  ' question = ',
+                  questionAnswer.question, ' context = ', questionAnswer.context, ' answer = ', questionAnswer.answer,
+                  'timeStamp = ', questionAnswer.timestamp)
             with sql.connect("questionAnswer.db") as con:
 
                 cur = con.cursor()
 
                 cur.execute(
                     "INSERT INTO Question_Answer VALUES (?,?,?,?,?)",
-                                                {'modelName': questionAnswer.modelName
-                                                 ,'question':questionAnswer.question
-                                                ,'context': questionAnswer.context
-                                                , 'answer': questionAnswer.answer
-                                                 ,'timestamp':questionAnswer.timestamp })
-                print('Inside dbHelper.getRecentlyAnsweredQuestionsList ','Data saved successfully')
+                    {'modelName': questionAnswer.modelName
+                        , 'question': questionAnswer.question
+                        , 'context': questionAnswer.context
+                        , 'answer': questionAnswer.answer
+                        , 'timestamp': questionAnswer.timestamp})
+                print('Inside dbHelper.getRecentlyAnsweredQuestionsList ', 'Data saved successfully')
                 return "Data saved successfully"
         except:
             con.rollback()
@@ -127,16 +145,35 @@ class dbHelper:
             con.close()
 
     # Create method to store all the logs
-    def saveDBLog(self):
-        return "Log saved succesfully"
+    def saveDBLog(self, appLogger):
+        try:
+            print('Inside dbHelper.saveDBLog')
+            with sql.connect("questionAnswer.db") as con:
+
+                cur = con.cursor()
+
+                cur.execute(
+                    "INSERT INTO APP_LOGGER VALUES (?,?,?,?,?)",
+                    {'modelName': appLogger.modelName
+                        , 'action_type': appLogger.action_type
+                        , 'action_details': appLogger.action_details
+                        , 'created_by': appLogger.created_by
+                        , 'created_time': appLogger.created_time})
+                print('Inside dbHelper.saveDBLog ', 'Data saved successfully')
+                return "Data saved successfully"
+        except:
+            con.rollback()
+            return "Exception occured while saving question answer data"
+        finally:
+            con.close()
 
     # Method to get mode details
-    def getModelDetails(self,modelName):
-        print('Inside dbHelper.getModelDetails for modelName = ' , modelName)
+    def getModelDetails(self, modelName):
+        print('Inside dbHelper.getModelDetails for modelName = ', modelName)
         try:
             with sql.connect("questionAnswer.db") as con:
                 con.row_factory = sql.Row
-                cur = con.execute("SELECT * FROM NLP_Models WHERE model = :modelName",{'modelName':modelName})
+                cur = con.execute("SELECT * FROM NLP_Models WHERE model = :modelName", {'modelName': modelName})
                 rows = cur.fetchall()
         except:
             con.rollback()
