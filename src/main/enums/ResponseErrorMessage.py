e61e0b2edf2a79a2489bfee3c65e162fa14cc994
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-05-25 00:36:43 -0400

    Update ResponseErrorMessage.py

--- src/main/enums/ResponseErrorMessage.py
+++ src/main/enums/ResponseErrorMessage.py
@@ -6,3 +6,5 @@ class ResponseErrorMessage(enum.Enum):
     ERORR_OCCUREED = "Some error occurred"
     START_TIME = "Start Time not specified"
     END_TIME = "EndTime not specified"
+    DATA_ALREDAY_EXISTS = "Model already exists"
+    DATA_DOES_NOT_ALREDAY_EXISTS = "Model to be deleted is not available"
