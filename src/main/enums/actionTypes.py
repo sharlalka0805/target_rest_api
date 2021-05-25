43a9baf4ea874e962cb775796764fa6cb90b9aed
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-05-25 15:19:56 -0400

    Create actionTypes.py

---
+++ src/main/enums/actionTypes.py
@@ -0,0 +1,7 @@
+import enum
+
+class actionTypes(enum.Enum):
+    ADD_MODEL = 'Add Model'
+    DELETE_MODEL = 'Delete Model'
+    FIND_ANSWER = 'Answer a question'
+    LIST_ANSWER = 'List recently asked questions'
