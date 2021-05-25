6ba5139d8838a522f9583b1e5a48db5539dc1d82
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-05-25 15:19:47 -0400

    Update appLogger.py

--- src/main/models/appLogger.py
+++ src/main/models/appLogger.py
@@ -1,21 +1,16 @@
 class appLogger:
     def __init__(self
                  ,model_name
-                 ,question_asked_time
-                 ,question_text
-                 ,answer_sent_time
-                 ,answer_text
-                 ,created_time
+                 ,action_type
+                 ,action_details
                  ,created_by
-                 ,modified_time
-                 ,modified_by):
+                 ,created_time
+                 ):
         self.model_name = model_name
-        self.question_asked_time = question_asked_time
-        self.question_text = question_text
-        self.answer_sent_time = answer_sent_time
-        self.answer_text = answer_text
-        self.created_time = created_time
+        self.action_type = action_type
+        self.action_details = action_details
         self.created_by = created_by
-        self.modified_time = modified_time
-        self.modified_by = modified_by
+        self.created_time = created_time
+
+
 
