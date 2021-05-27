300acd913289313792c2feff0f79b3a3a5f04ac2
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-05-27 17:23:47 -0400

    Create appLogger.py

---
+++ appLogger.py
@@ -0,0 +1,16 @@
+class appLogger:
+    def __init__(self
+                 ,model_name
+                 ,action_type
+                 ,action_details
+                 ,created_by
+                 ,created_time
+                 ):
+        self.model_name = model_name
+        self.action_type = action_type
+        self.action_details = action_details
+        self.created_by = created_by
+        self.created_time = created_time
+
+
+
