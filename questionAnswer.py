9f44d6bb8a9b341dda8daca39459b99524934f12
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-05-27 17:24:58 -0400

    Create questionAnswer.py

---
+++ questionAnswer.py
@@ -0,0 +1,8 @@
+class questionAnswer:
+    def __init__(self,timestamp,modelName,answer,question,context):
+        self.timestamp = timestamp
+        self.modelName = modelName
+        self.answer = answer
+        self.question = question
+        self.context = context
+
