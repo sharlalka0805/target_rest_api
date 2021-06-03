a685eb8041ff6ab4c82c14f290a37be45ba2d8f5
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-06-03 10:47:21 -0400

    Update dbHelper.py

--- dbHelper.py
+++ dbHelper.py
@@ -1,4 +1,6 @@
 import os
+import stat
+
 import psycopg2
 
 class dbHelper:
@@ -8,13 +10,41 @@ class dbHelper:
 
         # Format DB connection information
         sslmode = "sslmode=verify-ca"
-        sslrootcert = " sslrootcert={}".format(os.environ.get('PG_SSLROOTCERT'))
-        sslcert = " sslcert={}".format(os.environ.get('PG_SSLCERT'))
-        sslkey = " sslkey={}".format(os.environ.get('PG_SSLKEY'))
         hostaddr = " hostaddr={}".format(os.environ.get('PG_HOST'))
         user = " user=postgres"
         password = " password={}".format(os.environ.get('PG_PASSWORD'))
         dbname = " dbname=mgmt590"
+
+        sslrootcert_var = os.environ.get('PG_SSLROOTCERT')
+        sslrootcert_var = sslrootcert_var.replace('@', '=')
+        file = open("/server-ca.pem", "w")
+        file.write(sslrootcert_var)
+        file.close()
+        os.chmod("/server-ca.pem", stat.S_IRUSR)
+        os.chmod("/server-ca.pem", stat.S_IWUSR)
+
+        sslrootcert = "sslrootcert=/server-ca.pem"
+
+        sslcert_var = os.environ.get('PG_SSLCERT')
+        sslcert_var = sslcert_var.replace('@', '=')
+        file = open("/client-cert.pem", "w")
+        file.write(sslcert_var)
+        file.close()
+        os.chmod("/client-cert.pem", stat.S_IRUSR)
+        os.chmod("/client-cert.pem", stat.S_IWUSR)
+
+        sslcert = " sslcert=/client-cert.pem"
+
+        sslkey_var = os.environ.get('PG_SSLKEY')
+        sslkey_var = sslkey_var.replace('@', '=')
+        file = open("/client-key.pem", "w")
+        file.write(sslkey_var)
+        file.close()
+        os.chmod("/client-key.pem", stat.S_IRUSR)
+        os.chmod("/client-key.pem", stat.S_IWUSR)
+
+        sslkey = " sslkey=/client-key.pem"
+
 
         # Construct database connect string
         db_connect_string = " ".join([
