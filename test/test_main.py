b034c9046bc7c52e7f49ab2b854b48a4cf9e0536
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-06-03 10:38:50 -0400

    Create test_main.py

---
+++ test/test_main.py
@@ -0,0 +1,49 @@
+import os
+import tempfile
+import pytest
+from main import create_app
+
+#create test DB connection
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    # create the database and load test data
+    with app.app_context():
+        yield app
+
+@pytest.fixture
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
+
+
+ #Test Case for app route @app.route('/')
+def test_Welcome(client):
+    response = client.get("/")
+    print(response)
+    assert 200 == response.status_code
+
+# Test Case for app route @app.route('/models', =['GET', 'PUT','DELETE'])
+def test_getModels_GET(client):
+    response = client.get('/models')
+    assert response is not None
+
+def test_getModels_PUT(client):
+    modelData = {"name": "bert-tiny",
+                "tokenizer": "mrm8488/bert-tiny-5-finetuned-squadv2",
+                "model": "mrm8488/bert-tiny-5-finetuned-squadv2"}
+    reponse = client.put('/models',data = modelData)
+    assert reponse is not None
+
+def test_getModels_DELETE(client):
+    modelName = "saSS"
+    response = client.delete('/models?model='+modelName)
+    assert response is not None
+
+    # Test Case for app route @app.route('/answer', methods=['POST'])
+    # Test case for @app.route('/answer', methods=['GET'])
+    # Test case for
+
