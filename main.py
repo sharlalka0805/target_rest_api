f6b7a4be7346c6f6b0ebfecf95f0f97775d75650
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-06-03 19:01:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,261 +1,148 @@
-import os
-import time
-import sqlite3
-
-from transformers.pipelines import pipeline
 from flask import Flask
 from flask import request, jsonify
-
-# ---------------------#
-#  Create Flask App   #
-# ---------------------#
-
-# Create a variable that will hold our models in memory
-models = {}
-
-# The database file
-db = 'answers.db'
+from ResponseErrorMessage import ResponseErrorMessage
+from dbHelper import dbHelper
+from modelHelper import modelHelper
 
 
+################################  Route Functions - Start ###################################################
 def create_app():
-    # Create my flask app
-    app = Flask(__name__)
 
-    # Create a variable that will hold our models in memory
-    # models = {}
+    # Initialize the app
+    app = Flask(__name__,instance_relative_config=True)
 
-    # The database file
-    # db = 'answers.db'
+    @app.route('/')
+    def load_app():
+        name = 'User'
+        title = 'Welcome'
+        return "<html><h1> This service is UP and Running ... !!! </h1></html>"
 
-    # --------------#
-    #    ROUTES    #
-    # --------------#
+    # Method to get list of available models , Add a model , Delete an existing model
+    @app.route('/models', methods=['GET', 'PUT','DELETE'])
+    def getModelsList(modelName=None):
+            print("inside getModelsList in restAPIController ")
 
-    # Define a handler for the / path, which
-    # returns a message and allows Cloud Run to
-    # health check the API.
-    @app.route("/")
-    def hello_world():
-        return "<p>The question answering API is healthy!</p>"
+            if request.method == 'PUT':
+                print('Inside getModelsList: PUT')
 
-    # Define a handler for the /answer path, which
-    # processes a JSON payload with a question and
-    # context and returns an answer using a Hugging
-    # Face model.
-    @app.route("/answer", methods=['POST'])
-    def answer():
+                requestMessage = request.json
+                message = modelHelper.addModel(requestMessage)
 
-        # Get the request body data
-        data = request.json
+                if message == "Model added successfully":
+                    data = modelHelper.getModelList()
 
-        # Validate model name if given
-        if request.args.get('model') != None:
-            if not validate_model(request.args.get('model')):
-                return "Model not found", 400
+                    if data is not None:
+                        return jsonify(data)
+                    else:
+                        return modelHelper.errorResponseMessage(ResponseErrorMessage.DATA_NOT_FOUND.value)
+                else:
+                    return message
+            elif request.method == 'DELETE':
+                modelName = request.args.get('model')
+                print('Inside deleteModel = ', modelName)
 
-        # Answer the question
-        answer, model_name = answer_question(request.args.get('model'),
-                                             data['question'], data['context'])
-        timestamp = int(time.time())
+                if modelName is not None:
+                    message = modelHelper.deleteModel(modelName)
 
-        # Insert our answer in the database
-        con = sqlite3.connect(db)
-        cur = con.cursor()
-        sql = "INSERT INTO answers VALUES ('{question}','{context}','{model}','{answer}',{timestamp})"
-        cur.execute(sql.format(
-            question=data['question'].replace("'", "''"),
-            context=data['context'].replace("'", "''"),
-            model=model_name,
-            answer=answer,
-            timestamp=str(timestamp)))
-        con.commit()
-        con.close()
+                    if message == "Model deleted successfully":
+                        data = modelHelper.getModelList()
 
-        # Create the response body.
-        out = {
-            "question": data['question'],
-            "context": data['context'],
-            "answer": answer,
-            "model": model_name,
-            "timestamp": timestamp
-        }
+                        if data is not None:
+                            return jsonify(data)
+                        else:
+                            return modelHelper.errorResponseMessage(ResponseErrorMessage.DATA_NOT_FOUND.value)
+                    else:
+                        return message
+                else:
+                    return modelHelper.errorResponseMessage(ResponseErrorMessage.MISSING_INFO.value)
+            else:
+                print('Inside getModelsList: GET')
 
-        return jsonify(out)
+                data = modelHelper.getModelList()
+                if data is not None:
+                    return jsonify(data)
+                else:
+                    return modelHelper.errorResponseMessage(ResponseErrorMessage.DATA_NOT_FOUND.value)
 
-    # List historical answers from the database.
-    @app.route("/answer", methods=['GET'])
-    def list_answer():
 
-        # Validate timestamps
-        if request.args.get('start') == None or request.args.get('end') == None:
-            return "Query timestamps not provided", 400
+    # Answer a Question
+    @app.route('/answer', methods=['POST'])
+    def findAnswer(modelName=None):
+            modelName = request.args.get('model')
 
-        # Prep SQL query
-        if request.args.get('model') != None:
-            sql = "SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end} AND model == '{model}'"
-            sql_rev = sql.format(start=request.args.get('start'),
-                                 end=request.args.get('end'), model=request.args.get('model'))
-        else:
-            sql = 'SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end}'
-            sql_rev = sql.format(start=request.args.get('start'), end=request.args.get('end'))
+            if modelName == '':
+                modelName = 'bert-base-multilingual-uncased'
+                print('Inside  findAnswer using model =  ', modelName)
 
-        # Query the database
-        con = sqlite3.connect(db)
-        cur = con.cursor()
-        out = []
-        for row in cur.execute(sql_rev):
-            out.append({
-                "question": row[0],
-                "context": row[1],
-                "answer": row[2],
-                "model": row[3],
-                "timestamp": row[4]
-            })
-        con.close()
+            # Fetch the request JSON
+            requestMessage = request.json
 
-        return jsonify(out)
+            if requestMessage is None:
+                return modelHelper.errorResponseMessage(ResponseErrorMessage.Question_Not_Asked.value)
 
-    # List models currently available for inference
-    @app.route("/models", methods=['GET'])
-    def list_model():
+            # Call the method to answer the questions
+            answer = modelHelper.findAnswer(modelName, requestMessage)
 
-        # Get the loaded models
-        models_loaded = []
-        for m in models['models']:
-            models_loaded.append({
-                'name': m['name'],
-                'tokenizer': m['tokenizer'],
-                'model': m['model']
-            })
+            return jsonify(answer)
 
-        return jsonify(models_loaded)
+    # List recently answered questions
+    @app.route('/answer', methods=['GET'])
+    def listAnswers(modelName=None, startTime=None, endTime=None):
 
-    # Add a model to the models available for inference
-    @app.route("/models", methods=['PUT'])
-    def add_model():
+            modelName = request.args.get('model')
 
-        # Get the request body data
-        data = request.json
+            if modelName == '':
+                modelName = 'bert-base-multilingual-uncased'
+                print('Inside  findAnswer using model =  ', modelName)
 
-        # Load the provided model
-        if not validate_model(data['name']):
-            models_rev = []
-            for m in models['models']:
-                models_rev.append(m)
-            models_rev.append({
-                'name': data['name'],
-                'tokenizer': data['tokenizer'],
-                'model': data['model'],
-                'pipeline': pipeline('question-answering',
-                                     model=data['model'],
-                                     tokenizer=data['tokenizer'])
-            })
-            models['models'] = models_rev
+            startTime = request.args.get('start')
+            endTime = request.args.get('end')
 
-        # Get the loaded models
-        models_loaded = []
-        for m in models['models']:
-            models_loaded.append({
-                'name': m['name'],
-                'tokenizer': m['tokenizer'],
-                'model': m['model']
-            })
+            print('Inside restAPIHelper.listAnswers modelName = ',modelName,' startTime = ',startTime,' endTime = ',endTime)
+            if startTime == '':
+                return modelHelper.errorResponseMessage(ResponseErrorMessage.START_TIME.value)
 
-        return jsonify(models_loaded)
+            if endTime == '':
+                return modelHelper.errorResponseMessage(ResponseErrorMessage.END_TIME.value)
 
-    # Delete a model from the models available for inference
-    @app.route("/models", methods=['DELETE'])
-    def delete_model():
+            recentlyAnsweredQuestionsList = modelHelper.getRecentlyAnsweredQuestions(modelName, startTime, endTime)
 
-        # Validate model name if given
-        if request.args.get('model') == None:
-            return "Model name not provided in query string", 400
+            if recentlyAnsweredQuestionsList is not None:
+                return jsonify(recentlyAnsweredQuestionsList)
+            else:
+                return modelHelper.errorResponseMessage(ResponseErrorMessage.ERORR_OCCUREED.value)
 
-        # Error if trying to delete default model
-        if request.args.get('model') == models['default']:
-            return "Can't delete default model", 400
 
-        # Load the provided model
-        models_rev = []
-        for m in models['models']:
-            if m['name'] != request.args.get('model'):
-                models_rev.append(m)
-        models['models'] = models_rev
+    # Exception Handler
+    @app.errorhandler(Exception)
+    def handle_exception(error):
+            message = [str(x) for x in error.args]
+            success = False
+            response = {
+                'success': success,
+                'error': {
+                    'type': error.__class__.__name__,
+                    'message': message
+                }
+            }
 
-        # Get the loaded models
-        models_loaded = []
-        for m in models['models']:
-            models_loaded.append({
-                'name': m['name'],
-                'tokenizer': m['tokenizer'],
-                'model': m['model']
-            })
-
-        return jsonify(models_loaded)
-
+            return jsonify(response)
     return app
 
+################################  Route Functions - END ###################################################
 
-# --------------#
-#  FUNCTIONS   #
-# --------------#
-
-# Validate that a model is available
-def validate_model(model_name):
-    # Get the loaded models
-    model_names = []
-    for m in models['models']:
-        model_names.append(m['name'])
-
-    return model_name in model_names
-
-
-# Answer a question with a given model name
-def answer_question(model_name, question, context):
-    # Get the right model pipeline
-    if model_name == None:
-        for m in models['models']:
-            if m['name'] == models['default']:
-                model_name = m['name']
-                hg_comp = m['pipeline']
-    else:
-        for m in models['models']:
-            if m['name'] == model_name:
-                hg_comp = m['pipeline']
-
-    # Answer the answer
-    answer = hg_comp({'question': question, 'context': context})['answer']
-
-    return answer, model_name
-
-
-# Run main by default if running "python answer.py"
 if __name__ == '__main__':
-    # Create our Flask App
+
+    modelHelper = modelHelper()
+    dbHelper = dbHelper()
+
+    # Do the initial DB setup
+    message = dbHelper.createDatabase()
+    print(message)
+
+    # Create the app
     app = create_app()
 
-    # Initialize our default model.
-    models = {
-        "default": "distilled-bert",
-        "models": [
-            {
-                "name": "distilled-bert",
-                "tokenizer": "distilbert-base-uncased-distilled-squad",
-                "model": "distilbert-base-uncased-distilled-squad",
-                "pipeline": pipeline('question-answering',
-                                     model="distilbert-base-uncased-distilled-squad",
-                                     tokenizer="distilbert-base-uncased-distilled-squad")
-            }
-        ]
-    }
+    # Run our Flask app
+    app.run(host='0.0.0.0', port=8080, threaded=True,debug=True)
 
-    # Database setup
-    con = sqlite3.connect(db)
-    cur = con.cursor()
-    cur.execute('''CREATE TABLE answers
-               (question text, context text, model text, answer text, timestamp int)''')
-    con.commit()
-    con.close()
-
-    # Run our Flask app and start listening for requests!
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), threaded=True)
