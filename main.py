5e270bd78cfdab32d668cfdfce712caf50a1c344
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-06-03 21:46:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,151 +1,292 @@
+import pandas as pd
 from flask import Flask
-from flask import request, jsonify
-from ResponseErrorMessage import ResponseErrorMessage
-from dbHelper import dbHelper
-from modelHelper import modelHelper
+from flask import request
+from flask import jsonify
+import time
+from transformers import pipeline
+import psycopg2
+import os
+import stat
+# Format DB connection information
+sslmode = "sslmode=verify-ca"
 
+# Format DB connection information
+sslrootcert_var = os.environ.get('PG_SSLROOTCERT')
+sslrootcert_var = sslrootcert_var.replace('@', '=')
+file = open("/server-ca.pem", "w")
+file.write(sslrootcert_var)
+file.close()
+os.chmod("/server-ca.pem", stat.S_IRUSR)
+os.chmod("/server-ca.pem", stat.S_IWUSR)
+sslrootcert = "sslrootcert=/server-ca.pem"
 
-################################  Route Functions - Start ###################################################
+sslcert_var = os.environ.get('PG_SSLCERT')
+sslcert_var = sslcert_var.replace('@', '=')
+file = open("/client-cert.pem", "w")
+file.write(sslcert_var)
+file.close()
+os.chmod("/client-cert.pem", stat.S_IRUSR)
+os.chmod("/client-cert.pem", stat.S_IWUSR)
+sslcert = "sslcert=/client-cert.pem"
+
+sslkey_var = os.environ.get('PG_SSLKEY')
+sslkey_var = sslkey_var.replace('@', '=')
+file = open("/client-key.pem", "w")
+file.write(sslkey_var)
+file.close()
+os.chmod("/client-key.pem", stat.S_IRUSR)
+os.chmod("/client-key.pem", stat.S_IWUSR)
+sslkey = "sslkey=/client-key.pem"
+
+hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
+user = "user=postgres"
+password = "password={}".format(os.environ.get('PG_PASSWORD'))
+dbname = "dbname=mgmt590"
+
+# Construct database connect string
+db_connect_string = " ".join([
+      sslmode,
+      sslrootcert,
+      sslcert,
+      sslkey,
+      hostaddr,
+      user,
+      password,
+      dbname
+    ])
+
+models ={}
+# Initialize our default model.
+models = {
+    "default": "distilled-bert",
+    "models": [
+        {
+            "name": "distilled-bert",
+            "tokenizer": "distilbert-base-uncased-distilled-squad",
+            "model": "distilbert-base-uncased-distilled-squad",
+            "pipeline": pipeline('question-answering',
+                                 model="distilbert-base-uncased-distilled-squad",
+                                 tokenizer="distilbert-base-uncased-distilled-squad")
+        }
+    ]
+}
+
+# Create my flask app
 def create_app():
 
-    # Initialize the app
-    app = Flask(__name__,instance_relative_config=True)
+    app = Flask(__name__)
 
-    @app.route('/')
-    def load_app():
-        name = 'User'
-        title = 'Welcome'
-        return "<html><h1> This service is UP and Running ... !!! </h1></html>"
-
-    # Method to get list of available models , Add a model , Delete an existing model
-    @app.route('/models', methods=['GET', 'PUT','DELETE'])
-    def getModelsList(modelName=None):
-            print("inside getModelsList in restAPIController ")
-
-            if request.method == 'PUT':
-                print('Inside getModelsList: PUT')
-
-                requestMessage = request.json
-                message = modelHelper.addModel(requestMessage)
-
-                if message == "Model added successfully":
-                    data = modelHelper.getModelList()
-
-                    if data is not None:
-                        return jsonify(data)
-                    else:
-                        return modelHelper.errorResponseMessage(ResponseErrorMessage.DATA_NOT_FOUND.value)
-                else:
-                    return message
-            elif request.method == 'DELETE':
-                modelName = request.args.get('model')
-                print('Inside deleteModel = ', modelName)
-
-                if modelName is not None:
-                    message = modelHelper.deleteModel(modelName)
-
-                    if message == "Model deleted successfully":
-                        data = modelHelper.getModelList()
-
-                        if data is not None:
-                            return jsonify(data)
-                        else:
-                            return modelHelper.errorResponseMessage(ResponseErrorMessage.DATA_NOT_FOUND.value)
-                    else:
-                        return message
-                else:
-                    return modelHelper.errorResponseMessage(ResponseErrorMessage.MISSING_INFO.value)
-            else:
-                print('Inside getModelsList: GET')
-
-                data = modelHelper.getModelList()
-                if data is not None:
-                    return jsonify(data)
-                else:
-                    return modelHelper.errorResponseMessage(ResponseErrorMessage.DATA_NOT_FOUND.value)
+    def convert_to_dict(df):
+        result = []
+        for index, row in df.iterrows():
+            # result[index] = row.to_json()
+            result.append(dict(row))
+        return result
 
 
-    # Answer a Question
-    @app.route('/answer', methods=['POST'])
-    def findAnswer(modelName=None):
-            modelName = request.args.get('model')
+    @app.route("/")
+    def hello_world():
+        return "<p>The question answering API is healthy!</p>"
 
-            if modelName == '':
-                modelName = 'bert-base-multilingual-uncased'
-                print('Inside  findAnswer using model =  ', modelName)
+    # Define a handler for the /answer path, which
+    # processes a JSON payload with a question and
+    # context and returns an answer using a Hugging
+    # Face model.
+    @app.route("/answer", methods=['POST'])
+    def answer():
+        # Get the request body data
+        data = request.json
 
-            # Fetch the request JSON
-            requestMessage = request.json
+        # Validate model name if given
+        if request.args.get('model') != None:
+            if not validate_model(request.args.get('model')):
+                return "Model not found", 400
 
-            if requestMessage is None:
-                return modelHelper.errorResponseMessage(ResponseErrorMessage.Question_Not_Asked.value)
+        # Answer the question
+        answer, model_name = answer_question(request.args.get('model'),
+                                             data['question'], data['context'])
+        timestamp = int(time.time())
 
-            # Call the method to answer the questions
-            answer = modelHelper.findAnswer(modelName, requestMessage)
+        # Insert our answer in the database
+        con = psycopg2.connect(db_connect_string)
+        cur = con.cursor()
+        sql = "INSERT INTO answers VALUES ('{question}','{context}','{model}','{answer}',{timestamp})"
+        cur.execute(sql.format(
+            question=data['question'].replace("'", "''"),
+            context=data['context'].replace("'", "''"),
+            model=model_name,
+            answer=answer,
+            timestamp=str(timestamp)))
+        con.commit()
+        con.close()
 
-            return jsonify(answer)
+        # Create the response body.
+        out = {
+            "question": data['question'],
+            "context": data['context'],
+            "answer": answer,
+            "model": model_name,
+            "timestamp": timestamp
+        }
 
-    # List recently answered questions
-    @app.route('/answer', methods=['GET'])
-    def listAnswers(modelName=None, startTime=None, endTime=None):
-
-            modelName = request.args.get('model')
-
-            if modelName == '':
-                modelName = 'bert-base-multilingual-uncased'
-                print('Inside  findAnswer using model =  ', modelName)
-
-            startTime = request.args.get('start')
-            endTime = request.args.get('end')
-
-            print('Inside restAPIHelper.listAnswers modelName = ',modelName,' startTime = ',startTime,' endTime = ',endTime)
-            if startTime == '':
-                return modelHelper.errorResponseMessage(ResponseErrorMessage.START_TIME.value)
-
-            if endTime == '':
-                return modelHelper.errorResponseMessage(ResponseErrorMessage.END_TIME.value)
-
-            recentlyAnsweredQuestionsList = modelHelper.getRecentlyAnsweredQuestions(modelName, startTime, endTime)
-
-            if recentlyAnsweredQuestionsList is not None:
-                return jsonify(recentlyAnsweredQuestionsList)
-            else:
-                return modelHelper.errorResponseMessage(ResponseErrorMessage.ERORR_OCCUREED.value)
+        return jsonify(out)
 
 
-    # Exception Handler
-    @app.errorhandler(Exception)
-    def handle_exception(error):
-            message = [str(x) for x in error.args]
-            success = False
-            response = {
-                'success': success,
-                'error': {
-                    'type': error.__class__.__name__,
-                    'message': message
-                }
-            }
+    # List historical answers from the database.
+    @app.route("/answer", methods=['GET'])
+    def list_answer():
+        # Validate timestamps
+        if request.args.get('start') == None or request.args.get('end') == None:
+            return "Query timestamps not provided", 400
 
-            return jsonify(response)
+        # Prep SQL query
+        if request.args.get('model') != None:
+            sql = "SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end} AND model == '{model}'"
+            sql_rev = sql.format(start=request.args.get('start'),
+                                 end=request.args.get('end'), model=request.args.get('model'))
+        else:
+            sql = 'SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end}'
+            sql_rev = sql.format(start=request.args.get('start'), end=request.args.get('end'))
+
+        # Query the database
+        con = psycopg2.connect(db_connect_string)
+        cur = con.cursor()
+        out = []
+        for row in cur.execute(sql_rev):
+            out.append({
+                "question": row[0],
+                "context": row[1],
+                "answer": row[2],
+                "model": row[3],
+                "timestamp": row[4]
+            })
+        con.close()
+
+        return jsonify(out)
+
+
+    # List models currently available for inference
+    @app.route("/models", methods=['GET'])
+    def list_model():
+        # Get the loaded models
+        models_loaded = []
+        for m in models['models']:
+            models_loaded.append({
+                'name': m['name'],
+                'tokenizer': m['tokenizer'],
+                'model': m['model']
+            })
+
+        return jsonify(models_loaded)
+
+
+    # Add a model to the models available for inference
+    @app.route("/models", methods=['PUT'])
+    def add_model():
+        # Get the request body data
+        data = request.json
+
+        # Load the provided model
+        if not validate_model(data['name']):
+            models_rev = []
+            for m in models['models']:
+                models_rev.append(m)
+            models_rev.append({
+                'name': data['name'],
+                'tokenizer': data['tokenizer'],
+                'model': data['model'],
+                'pipeline': pipeline('question-answering',
+                                     model=data['model'],
+                                     tokenizer=data['tokenizer'])
+            })
+            models['models'] = models_rev
+
+        # Get the loaded models
+        models_loaded = []
+        for m in models['models']:
+            models_loaded.append({
+                'name': m['name'],
+                'tokenizer': m['tokenizer'],
+                'model': m['model']
+            })
+
+        return jsonify(models_loaded)
+
+
+    # Delete a model from the models available for inference
+    @app.route("/models", methods=['DELETE'])
+    def delete_model():
+        # Validate model name if given
+        if request.args.get('model') == None:
+            return "Model name not provided in query string", 400
+
+        # Error if trying to delete default model
+        if request.args.get('model') == models['default']:
+            return "Can't delete default model", 400
+
+        # Load the provided model
+        models_rev = []
+        for m in models['models']:
+            if m['name'] != request.args.get('model'):
+                models_rev.append(m)
+        models['models'] = models_rev
+
+        # Get the loaded models
+        models_loaded = []
+        for m in models['models']:
+            models_loaded.append({
+                'name': m['name'],
+                'tokenizer': m['tokenizer'],
+                'model': m['model']
+            })
+
+        return jsonify(models_loaded)
+
+
+    # --------------#
+    #  FUNCTIONS   #
+    # --------------#
+
+    # Validate that a model is available
+    def validate_model(model_name):
+        # Get the loaded models
+        model_names = []
+        for m in models['models']:
+            model_names.append(m['name'])
+
+        return model_name in model_names
+
+
+    # Answer a question with a given model name
+    def answer_question(model_name, question, context):
+        # Get the right model pipeline
+        if model_name == None:
+            for m in models['models']:
+                if m['name'] == models['default']:
+                    model_name = m['name']
+                    hg_comp = m['pipeline']
+        else:
+            for m in models['models']:
+                if m['name'] == model_name:
+                    hg_comp = m['pipeline']
+
+        # Answer the answer
+        answer = hg_comp({'question': question, 'context': context})['answer']
+
+        return answer, model_name
     return app
 
-################################  Route Functions - END ###################################################
-
+# Run main by default if running "python answer.py"
 if __name__ == '__main__':
 
-    modelHelper = modelHelper()
-    dbHelper = dbHelper()
-
-    # Do the initial DB setup
-    message = dbHelper.createDatabase()
-    print(message)
-
-    message_2 = dbHelper.insertInitialData()
-    print(message_2)
-
-    # Create the app
     app = create_app()
+    # Database setup
+    con = psycopg2.connect(db_connect_string)
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS answers
+               (question text, context text, model text, answer text, timestamp int)''')
+    con.commit()
+    con.close()
 
-    # Run our Flask app
-    app.run(host='0.0.0.0', port=8080, threaded=True,debug=True)
-
+    # Run our Flask app and start listening for requests!
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), threaded=True)
