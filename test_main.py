9a40de22046bff1c6f0d32c24396227da45cddb3
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-06-03 22:57:01 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,10 @@
 import pytest
 from main import create_app
-
 import time
-import sqlite3
 
+import sqlite3
 timestamp = int(time.time())
+
 
 @pytest.fixture
 def client():
@@ -12,9 +12,15 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+
+# test /models GET
 def test_health(client):
     r = client.get("/")
     assert 200 == r.status_code
+
+
+# test /models GET
 def test_addModel(client):
     print("Testing Add Model functionality")
     # Test add a model functionality
@@ -26,12 +32,14 @@ def test_addModel(client):
     r = client.put("/models", json=payload)
     assert 200 == r.status_code
 
+
+# test /models GET
 def test_getModel(client):
-    # test /models GET
     r = client.get("/models")
     assert 200 == r.status_code
 
+
+# Test /models DELETE
 def test_deleteModel(client):
-    # Test /models DELETE
     r = client.delete("/models?model=bert-tiny")
     assert 200 == r.status_code
