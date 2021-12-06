67a45e253bdd07e395226c6d45fb0af5383d7343
Shipra Harlalka <74998715+sharlalka0805@users.noreply.github.com>
2021-12-05 22:10:06 -0500

    commit

--- actionTypes.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class actionTypes(enum.Enum):
-    ADD_MODEL = 'Add Model'
-    DELETE_MODEL = 'Delete Model'
-    FIND_ANSWER = 'Answer a question'
-    LIST_ANSWER = 'List recently asked questions'
--- questionAnswer.py
+++
@@ -1,8 +0,0 @@
-class questionAnswer:
-    def __init__(self,timestamp,modelName,answer,question,context):
-        self.timestamp = timestamp
-        self.modelName = modelName
-        self.answer = answer
-        self.question = question
-        self.context = context
-
